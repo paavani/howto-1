a5eae8e9133c8f5468b031746a4f279834900126
koti <ekanna.net@gmail.com>
2015-04-16 09:58:29 +0530

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -14,5 +14,26 @@ $$
    Values ('2015-01-01'::date),('2015-02-01'),('2015-03-01') --important 
 $$
 )
-
+-- here columns names can be whatever you want but order of the columns is important
 AS ct(mgroup text, "2005-01-01" numeric, "2015-02-01" numeric, "2015-03-01" numeric);
+
+--later we can split conacatanated column as below;
+select split_part(mgroup, '@', 1) as mis, split_part(mgroup, '@', 2) as smis, aboveTable.* from aboveTable;
+
+
+-- suppose if smis is unique acroos all mis then we can do like below
+select * from crosstab(
+$$
+   -- column order for select rowid, extraColumn(s), category, values
+   select smis, mis, rmonth, sum(cbusdeq) -- all extracolumn(s) should be between rowid and category. Here extracolumn is mis.
+   from zmis.tb
+   where bp='B' and (rmonth = '2015-01-01' or rmonth='2015-02-01' or rmonth='2015-03-01')
+   group by smis, mis, rmonth
+   order by 1,2 
+$$,
+$$
+   Values ('2015-01-01'::date),('2015-02-01'),('2015-03-01') --important 
+$$
+)
+-- here columns names can be whatever you want but order of the columns is important
+AS ct(smis text, mis text, "2005-01-01" numeric, "2015-02-01" numeric, "2015-03-01" numeric); 
